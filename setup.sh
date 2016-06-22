233c68b6f90fa263cafd0ab33ed9bd6ab9cc2a27
VZIKL <503519049@qq.com>
2016-06-22 10:35:17 +0800

    setup

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if which apt-get >/dev/null; then
+        sudo apt-get install vim catgs curl python-dev python3-dev xclip astyle xdg-utils nodejs
+elif which emerge >/dev/null;then
+        sudo emerge -av vim curl python-dev python3-dev astyle ctags xdg-utils nodejs
+fi
+sudo easy_install -ZU autopep8
+mv -f ~/.vim ~/vim_old
+mv ~/.vimrc ~/vim_old/vimrc
+cd ~/ && git clone https://github.com/VZIKL/vimrc.git
+mv ~/vimrc/vimrc ~/.vimrc
+mv -f ~/vimrc ~/.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "安装vim插件中" > VZIKL
+vim VZIKL -c "BundleInstall" -c "q" -c "q"
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --all
+rm VZIKL
+echo “安装完成
