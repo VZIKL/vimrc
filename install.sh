124c05b2237fc862c263704a1bfcb8c88fda7c1a
VZIKL <cvzikl@gmail.com>
2016-09-22 15:55:05 +0800

    rebuild gentoo

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 if which apt-get >/dev/null; then
     sudo apt-get update
-    sudo apt-get install vim ctags curl python-dev python3-dev xclip astyle xdg-utils nodejs git cmake build-essential nodejs-legacy python-setuptools libpython2.7-dbg libpython2.7-dev libpython-dev
+    sudo apt-get install vim ctags curl python-dev python3-dev xclip astyle xdg-utils nodejs git cmake build-essential nodejs-legacy python-setuptools libpython2.7-dbg libpython2.7-dev libpython-dev monodevelop cargo
 elif which pacman >/dev/null; then
     sudo pacman -Syu
     sudo pacman -S go cargo nodejs npm monodevelop base-level python2 python
+elif which emerge >/dev/null; then
+    sudo emerge -avuDN world
+    sudo emerge -av dev-util/ctags vim net-misc/curl x11-misc/xclip dev-util/astyle x11-misc/xdg-utils net-libs/nodejs dev-vcs/git dev-util/cmake dev-python/setuptools dev-util/monodevelop dev-util/cargo
 fi
 mv -f ~/.vim ~/vim_old && mv ~/.vimrc ~/vim_old/vimrc
 cd ~ && git clone https://github.com/VZIKL/vimrc
