127db5ab0a48d07056b72bc186dda13c672fcef0
VZIKL <VZIKL@users.noreply.github.com>
2016-10-04 13:06:53 +0800

    fixed

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+mv ~/.vimrc ~/.vim/vimrc
 cd ~/.vim && git pull
 mv ~/.vim/vimrc ~/.vimrc
 cd ~ && vim -c "BundleUpdate" -c "q" -c "q"
 echo "Success Update"
+echo "Maybe you will complete ycm"
