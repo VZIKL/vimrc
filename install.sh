f7cd008121f12870528c45159be85978d858535a
VZIKL <cvzikl@gmail.com>
2016-11-11 11:35:49 +0800

    fix bug

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if which apt-get >/dev/null; then
     sudo apt-get update
-    sudo apt-get install vim ctags curl python-dev python3-dev xclip astyle xdg-utils nodejs git cmake build-essential nodejs-legacy python-setuptools libpython2.7-dbg libpython2.7-dev libpython-dev monodevelop cargo mono-devel mono-xbild mono-complete pyflakes
+    sudo apt-get install vim ctags curl python-dev python3-dev xclip astyle xdg-utils nodejs git cmake build-essential nodejs-legacy python-setuptools libpython2.7-dbg libpython2.7-dev libpython-dev monodevelop cargo mono-devel mono-xbild mono-complete pyflakes clang-format
 elif which pacman >/dev/null; then
     sudo pacman -Syu
     sudo pacman -S go cargo nodejs npm monodevelop base-level python2 python
@@ -13,11 +13,15 @@ mv -f ~/.vim ~/vim_old && mv ~/.vimrc ~/vim_old/vimrc
 cd ~ && git clone https://github.com/VZIKL/vimrc
 mv ~/vimrc/vimrc ~/.vimrc && mv -f ~/vimrc ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/Valloric/YouCompleteMe ~/.vim/bundle/YouCompleteMe
+cd ~/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
+./install.py --clang-completer
 vim -c "BundleInstall" -c "q" -c "q"
 cp ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py ~
 cp ~/.vim/bundle/vim-instant-markdown/after/ftplugin/markdown/instant-markdown.vim ~/.vim/plugin/instant-markdown.vim
 sudo npm -g install instant-markdown-d && cd ~
+clang-format  -style=google -dump-config > .clang-format
 chmod +x update.sh
 echo 安装完成
 echo "编译ycm cd ~/.vim/bundle/YouCompleteMe && ./install.py -all"
-echo "./install.py --clang-completer"
