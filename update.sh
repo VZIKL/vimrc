6187158a9bde0d15a7169b8272b11913946e7d57
VZIKL <cvzikl@gmail.com>
2017-01-11 01:32:15 +0800

    fixed

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ mv ~/.vimrc ~/.vim/vimrc
 cd ~/.vim && git pull
 mv ~/.vim/vimrc ~/.vimrc
 vim_info=$(vim --version|head -1|cut -d ' ' -f 5)
-if  [vim_info >=8.0];then
+if  [ vim_info >=8.0 ];then
     cd ~/.vim/pack/plugin/opt
     ls | xargs -I{} git -C {} pull
 else
