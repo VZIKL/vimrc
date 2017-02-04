b4f7749f0fbe578651ce0ffb8d81c7b76bb3b4a3
VZIKL <cvzikl@gmail.com>
2017-02-04 21:46:23 +0800

    install

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+sudo emerge -av dev-util/ctags vim net-misc/curl x11-misc/xclip dev-util/astyle x11-misc/xdg-utils net-libs/nodejs dev-vcs/git dev-util/cmake dev-python/setuptools dev-util/monodevelop dev-util/cargo pyflakes dev-python/pip clang llvm
 git clone https://github.com/VZIKL/vimrc ~/.vim
 mv ~/.vim/vimrc ~/.vimrc 
