51dac54d64f243d6c7bf94a9204c2d3e14cd1df1
VZIKL <cvzikl@gmail.com>
2017-04-19 18:06:15 +0800

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo emerge -av dev-util/ctags vim net-misc/curl x11-misc/xclip dev-util/astyle x11-misc/xdg-utils net-libs/nodejs dev-vcs/git dev-util/cmake dev-python/setuptools dev-util/monodevelop dev-util/cargo pyflakes dev-python/pip clang llvm
 git clone https://github.com/VZIKL/vimrc ~/.vim
+cd ~/.vim && git submodule update --init
 mv ~/.vim/vimrc ~/.vimrc 
