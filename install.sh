117cf523be1aad67873278e4f1dad41da4fdc776
VZIKL <cvzikl@gmail.com>
2017-04-28 21:20:42 +0800

    update

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 if which emerge &> /dev/null;then
 sudo emerge -av dev-util/ctags vim net-misc/curl x11-misc/xclip dev-util/astyle x11-misc/xdg-utils net-libs/nodejs dev-vcs/git dev-util/cmake dev-python/setuptools dev-util/monodevelop dev-util/cargo pyflakes dev-python/pip clang llvm
 elif which apt-get &> /dev/null;then
-sudo apt-get install ctags vim curl xclip astyle xdg-utils nodejs git cmake setuptools monodevelop cargo pyflakes pip clang llvm
+sudo apt-get install ctags vim curl xclip astyle xdg-utils nodejs git cmake python-setuptools monodevelop cargo pyflakes python-pip clang llvm
 fi
 git clone https://github.com/VZIKL/vimrc ~/.vim
 cd ~/.vim && git submodule update --init
