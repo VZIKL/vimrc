717f681919149b43c56a46401c5d6e8ae1ec3bf4
VZIKL <cvzikl@gmail.com>
2017-06-02 19:57:10 +0800

    install

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ fi
 git clone https://github.com/VZIKL/vimrc ~/.vim
 cd ~/.vim && git submodule update --init --recursive
 mv ~/.vim/vimrc ~/.vimrc 
-cp .vim/
 cp ~/.vim/pack/plugin/opt/YouCompleteMe/third_party/ycmd/examples/.ycm_extra_conf.py ~
+cp ~/.vim/pack/plugin/opt/slimv/syntax/lisp/slimv-syntax-lisp.vim ~/.vim/syntax/
+
 rm ~/install.sh
